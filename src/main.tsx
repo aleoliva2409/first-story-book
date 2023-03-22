2775ed01604b4cc35aff75a8d91e93a7c5c0298c
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2023-03-22 17:58:24 -0300

    feat: package.json updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { Button } from './stories/Button';
-import { Header } from './stories/Header';
-import { Page } from './stories/Page';
+import Button from './stories/Button';
+import Header from './stories/Header';
+import Page from './stories/Page';
 import MyLabel from './components/MyLabel';
 
 export { Button, Header, Page, MyLabel };
